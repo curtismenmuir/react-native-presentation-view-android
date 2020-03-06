9fd9e3fa6f9ae4643ed909155902cc8c6cdebd18
Curtis Menmuir <curtismenmuir@gmail.com>
2020-03-06 00:26:23 +0000

    Added ScrollView to presentation to demo DeviceEventEmitter events to presentation screen.

--- example/presentation.js
+++ example/presentation.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
-import { DeviceEventEmitter, StyleSheet, View } from "react-native";
-
+import {
+  DeviceEventEmitter,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  View
+} from "react-native";
 class PresentationView extends Component {
   constructor(props) {
     super(props);
@@ -8,22 +13,66 @@ class PresentationView extends Component {
       position: 0
     };
   }
-
   componentWillMount() {
     DeviceEventEmitter.addListener("skip", data => {
       this.updatePosition();
     });
   }
-
+  componentDidUpdate() {
+    this.scrollToPosition(this.state.position);
+  }
+  componentWillUnmount() {
+    this.scrollView = null;
+  }
+  updatePosition() {
+    const position = this.state.position === 4 ? 0 : this.state.position + 1;
+    this.setState({ position: position });
+  }
+  scrollToPosition = position => {
+    const scrollYPos = Dimensions.get("window").height * position;
+    this.scrollView.scrollTo({ y: scrollYPos, animated: true });
+  };
   render() {
-    return <View style={styles.container} />;
+    return (
+      <ScrollView
+        style={styles.container}
+        ref={scrollView => {
+          this.scrollView = scrollView;
+        }}
+      >
+        <View style={styles.panelOne} />
+        <View style={styles.panelTwo} />
+        <View style={styles.panelThree} />
+        <View style={styles.panelFour} />
+        <View style={styles.panelFive} />
+      </ScrollView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "green"
+    flex: 1
+  },
+  panelOne: {
+    backgroundColor: "#0AA200",
+    height: Dimensions.get("window").height
+  },
+  panelTwo: {
+    backgroundColor: "#FE8603",
+    height: Dimensions.get("window").height
+  },
+  panelThree: {
+    backgroundColor: "#A60067",
+    height: Dimensions.get("window").height
+  },
+  panelFour: {
+    backgroundColor: "#fb3d38",
+    height: Dimensions.get("window").height
+  },
+  panelFive: {
+    backgroundColor: "#395e66",
+    height: Dimensions.get("window").height
   }
 });
 
